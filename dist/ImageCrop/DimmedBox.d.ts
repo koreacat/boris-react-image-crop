7694ff47185443e8c7e7636cc5b75c0e2faaadc8
koreacat <kj9966@naver.com>
2023-01-09 17:34:44 +0900

    image crop

--- dist/ImageCrop/DimmedBox.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import { Size, Point } from './data';
-interface DimmedBoxProps {
-    imgSize: Size;
-    setOffset: (offset: ((prev: Point) => Point) | Point) => void;
-    setCropBoxSize: (cropBoxSize: Size) => void;
-}
-declare const DimmedBox: ({ imgSize, setOffset, setCropBoxSize }: DimmedBoxProps) => JSX.Element;
-export default DimmedBox;
--- dist/index.js
+++
@@ -1,1 +0,0 @@
-export { default as ImageCrop } from "./ImageCrop";
